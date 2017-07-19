165c3630c6f92d3b6b36431da41d3b85dbdecb0b
lyubich <anastasia.lyubich@gmail.com>
2017-07-19 09:34:32 +0300

    Add dependencies for Hibernate
    Add bad commit
    
    Add dependencies for Hibernate

--- src/main/java/account/Application.java
+++ src/main/java/account/Application.java
@@ -1,35 +1,23 @@
 package account;
 
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.annotation.Bean;
-
-
-import java.util.Arrays;
-import java.util.Locale;
+import org.springframework.boot.autoconfigure.data.jpa.JpaRepositoriesAutoConfiguration;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 
 @SpringBootApplication
+//        (exclude = JpaRepositoriesAutoConfiguration.class)
 public class Application {
+//    extends SpringBootServletInitializer
 
     public static void main(String[] args) {
-        SpringApplication.run(Application.class, args);
-    }
+        SpringApplication app = new SpringApplication(Application.class);
+        app.run(args);
+     }
 
-    @Bean
-    public CommandLineRunner commandLineRunner(ApplicationContext ctx) {
-        return args -> {
-
-            System.out.println("Let's inspect the beans provided by Spring Boot:");
-
-            String[] beanNames = ctx.getBeanDefinitionNames();
-            Arrays.sort(beanNames);
-            for (String beanName : beanNames) {
-                System.out.println(beanName);
-            }
-
-        };
-    }
-
+//    @Override
+//    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+//        return application.sources(Application.class);
+//    }
 }
